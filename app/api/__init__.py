80476dc7b9ebb44fc9db0c805bfbd03abf993c0d
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-08-31 23:21:16 +0530

    api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
+from app.api.user_routes import user_router
 from app.api.health_check_routes import health_check_router
 
 routers = APIRouter()
 
-routers.include_router(router=health_check_router, tags=['Health Check'])
+routers.include_router(router=user_router, tags=["Users"])
+routers.include_router(router=health_check_router, tags=["Health Check"])
---
+++ app/api/user_routes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends
+from app.core.db.postgresql import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.users import UserCreate, UserOut
+from app.services.user_service import UserService
+from app.repository.user_repository import UserRepository
+from app.core.messaging.kafka.producer import KafkaProducer
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+producer = KafkaProducer()
+
+
+def get_service(db: AsyncSession = Depends(get_db)) -> UserService:
+    repo = UserRepository(db)
+    return UserService(repo=repo, producer=producer)
+
+
+@router.post("", response_model=UserOut, status_code=201)
+async def create_user(payload: UserCreate, service: UserService = Depends(get_service)):
+    return await service.create_user(payload)
+
+
+@router.get("/{user_id}", response_model=UserOut | None)
+async def get_user(user_id: int, service: UserService = Depends(get_service)):
+    return await service.get_user(user_id)
+
+
+@router.get("", response_model=list[UserOut])
+async def list_users(service: UserService = Depends(get_service)):
+    return await service.list_users()
