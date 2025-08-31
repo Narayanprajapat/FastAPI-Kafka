04884935bd16927a1dafe6271437c24cb17b48cf
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-08-31 22:44:45 +0530

    logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,12 +1,10 @@
 import logging
 
 
-packages_to_surpass = [
-    "fastapi"
-]
+packages_to_surpass = ["fastapi"]
 
 for package in packages_to_surpass:
     logging.getLogger(package).setLevel(level=logging.INFO)
-    
-    
+
+
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(message)s")
