6e6b19cc043b6758a7f9bd78dac2df67dfe2c08f
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-08-31 22:45:16 +0530

    enums

--- app/core/config/enums.py
+++ app/core/config/enums.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
+
 class EventsName(Enum):
-    USER_EVENTS='USER_EVENTS'
-    ALL_EVENTS = {
-        USER_EVENTS
-    }
+    USER_EVENTS = "USER_EVENTS"
+    ALL_EVENTS = {USER_EVENTS}
