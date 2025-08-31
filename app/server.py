f66b95d3212f37cd6aa486d2f2064a871ec9b790
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-08-31 19:11:33 +0530

    server.py

--- app/server.py
+++ app/server.py
@@ -1,16 +1,23 @@
-
+import asyncio
 from app.api import routers
+from app.utils.logger import logging
+from app.core.messaging.kafka.consumer import consume_messages
 
 from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
 
+logger = logging.getLogger(name='server.py')
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    print('Starting')
+    logger.info('Starting')
+    asyncio.create_task(consume_messages())
+    await consume_messages()
     yield
-    print('Stopping')
+    logger.info('Stopping')
 
 
 app = FastAPI(lifespan=lifespan)
@@ -18,18 +25,20 @@ app = FastAPI(lifespan=lifespan)
 
 @app.middleware("http")
 async def log_middleware(request: Request, call_next):
-    print(f"Accessing {request.url.path}")
+    logger.info(f"Accessing {request.url.path}")
     response = await call_next(request)
-    print(f"Finished {request.url.path}")
+    logger.info(f"Finished {request.url.path}")
     return response
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[],          # list of allowed origins
-    allow_credentials=True,
-    allow_methods=["*"],            # ["GET", "POST"] if you want to restrict
-    allow_headers=["*"],            # ["Authorization", "Content-Type"] if restricted)
+    **{
+        "allow_origins":[],          # list of allowed origins
+        "allow_credentials":True,
+        "allow_methods":["*"],            # ["GET", "POST"] if you want to restrict
+        "allow_headers":["*"],            # ["Authorization", "Content-Type"] if restricted)
+    }
 )
 
 
