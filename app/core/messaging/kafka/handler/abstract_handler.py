6d37f195425cfe85493d3d22925c5908dd4920a7
Narayan Prajapat <narayanprajapat00099@gmail.com>
2025-08-31 19:11:54 +0530

    kafka.handlers

---
+++ app/core/messaging/kafka/handler/abstract_handler.py
@@ -0,0 +1,8 @@
+from typing import List
+from abc import ABC, abstractmethod
+
+
+class Handler(ABC):
+    @abstractmethod
+    def execute(self, event_data: List[dict])-> None:
+        pass
---
+++ app/core/messaging/kafka/handler/user_event_handler.py
@@ -0,0 +1,13 @@
+from app.utils.logger import logging
+from app.core.messaging.kafka.handler.abstract_handler import Handler
+
+
+logger = logging.getLogger('kafka.user_event_handler')
+
+
+class UserEventHandler(Handler):
+    def __init__(self):
+        super().__init__()
+    
+    def execute(self, event_data):
+        logger.info("Execute user event handler method")
